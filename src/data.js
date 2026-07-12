174711fa1c8402756229bbc0e5f760fe2f6ce658
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-12 10:03:49 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,52 +1,51 @@
 const shoes = [
   {
     id: 1,
-    name: " Adidas Ultraboost",
+    name: "Adidas Ultraboost",
     price: 4999,
-    image: "public/shoe-1.jpg"
+    image: "/shoe-1.jpg"
   },
   {
     id: 2,
     name: "Nike Air Max",
     price: 5999,
-    image: "public/shoe-2.jpg"
+    image: "/shoe-2.jpg"
   },
   {
     id: 3,
     name: "Puma RS-X",
     price: 4499,
-    image: "public/shoe-3.jpg"
+    image: "/shoe-3.jpg"
   },
   {
     id: 4,
     name: "SparX",
     price: 3999,
-    image: "public/shoe-4.jpg"
+    image: "/shoe-4.jpg"
   },
   {
     id: 5,
     name: "Bersache",
     price: 4299,
-    image: "public/shoe-5.jpg"
+    image: "/shoe-5.jpg"
   },
   {
     id: 6,
     name: "New Balance 574",
     price: 5499,
-    image: "public/shoe-6.jpg"
+    image: "/shoe-6.jpg"
   },
-  
   {
     id: 7,
     name: "RedTape",
     price: 3799,
-    image: "public/shoe-7.jpg"
+    image: "/shoe-7.jpg"
   },
   {
     id: 8,
-    name: "Millionaire walk",
+    name: "Millionaire Walk",
     price: 3399,
-    image: "public/shoe-8.jpg"
+    image: "/shoe-8.jpg"
   }
 ];
 
